b2677a300b40ebd0e6209adabcebffed3ea81d37
Frederic Nowak <fredenow@amazon.com>
2022-11-14 17:07:21 +0100

    Add license source headers

--- src/pbdg/events.py
+++ src/pbdg/events.py
@@ -1,3 +1,6 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
 from os.path import exists
 import random, uuid
 from datetime import datetime, timedelta
